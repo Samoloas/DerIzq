7a6687883a8ddf0c06dbef2a90390f0b6deb6689
Samoloas <135927616+Samoloas@users.noreply.github.com>
2024-05-02 07:38:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("APP_CERR")
+client1= paho.Client("Gestos")
 client1.on_message = on_message
 client1.on_publish = on_publish
 client1.connect(broker,port)
@@ -56,9 +56,9 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
-      client1.publish("IMIA","{'gesto': 'derecha'}",qos=0, retain=False)
+      client1.publish("DEIZ","{'gesto': 'derecha'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Cerrando')
-      client1.publish("IMIA","{'gesto': 'Izquierda'}",qos=0, retain=False)
+      client1.publish("DEIZ","{'gesto': 'Izquierda'}",qos=0, retain=False)
       time.sleep(0.2)  
